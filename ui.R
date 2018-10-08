d8e471d3b00a4fb04333152df83347f52d6cd3da
LachMitchell <35942935+LachMitchell@users.noreply.github.com>
2018-10-08 12:21:20 +1000

    Update ui.R

--- ui.R
+++ ui.R
@@ -16,7 +16,7 @@ shinyUI(fluidPage(
                numericInput("fVO2","Final VO2 Pk",5000),
                numericInput("iPCR","Initial [PCr] (mM/kg)",27.75),
                numericInput("tauPCr","Tau (PCr)",23.3),
-               numericInput("tauVO2","Tau (PCr)",22.7),
+               numericInput("tauVO2","Tau (VO2)",22.7),
                numericInput("MassRatio","Body Mass:Active Muscle Mass Ratio",.3),
                numericInput("TD1","Time Delay 1",5.49),
                numericInput("LaC","Lactate:ml O2 Ratio",2.71)
